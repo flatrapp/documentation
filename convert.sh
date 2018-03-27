72b9c209b35aadae192ff0f8ff5d63e623fa2ec9
Daniel Schaefer <git@danielschaefer.me>
2018-03-27 10:40:18 +0700

    Add report

--- convert.sh
+++ convert.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p pandoc
+#! nix-shell -i bash -p pandoc texlive.combined.scheme-small
 
-pandoc -t revealjs -s -o presentation.html presentation.md -V revealjs-url=http://lab.hakim.se/reveal-js -V theme=solarized
+pandoc -t revealjs -s -o presentation.html presentation.md -V revealjs-url=./reveal.js/ -V theme=solarized
+pandoc report.md --pdf-engine=pdflatex -o report.pdf
